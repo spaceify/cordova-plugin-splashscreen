a4d04d13b379873a5a13250222e4a2d4f88ed9a9
Petri Savolainen <petri.savolainen@helsinki.fi>
2021-06-06 12:03:32 +0300

    remove scaling

--- src/android/SplashScreen.java
+++ src/android/SplashScreen.java
@@ -303,6 +303,8 @@ public class SplashScreen extends CordovaPlugin {
                 // TODO: Use the background color of the webView's parent instead of using the preference.
                 //splashImageView.setBackgroundColor(preferences.getInteger("backgroundColor", Color.BLACK));
 
+                splashImageView.setScaleType(ImageView.ScaleType.CENTER);
+                /*
                 if (isMaintainAspectRatio()) {
                     // CENTER_CROP scale mode is equivalent to CSS "background-size:cover"
                     splashImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
@@ -311,6 +313,7 @@ public class SplashScreen extends CordovaPlugin {
                     // FIT_XY scales image non-uniformly to fit into image view.
                     splashImageView.setScaleType(ImageView.ScaleType.FIT_XY);
                 }
+                */
 
                 // Create and show the dialog
                 splashDialog = new Dialog(context, android.R.style.Theme_Translucent_NoTitleBar);
